9e164c9c067615b5c949e351a79227c516d8d3de
jbmakib <mdfoujolmobin@gmail.com>
2021-10-18 18:18:28 +0600

    private route added

---
+++ src/Pages/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Redirect } from "react-router";
+import useAuth from "../../hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+    const { user } = useAuth();
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                user.accessToken ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{ pathname: "/login", state: { from: location } }}
+                    ></Redirect>
+                )
+            }
+        ></Route>
+    );
+};
+
+export default PrivateRoute;
