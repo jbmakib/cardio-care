40475b1d8327ce81ea436aabffb24c4114c1d6e4
jbmakib <mdfoujolmobin@gmail.com>
2021-10-18 15:27:55 +0600

    AuthProvider added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import AuthProvider from "./contexts/AuthProvider";
 import Home from "./Pages/Home/Home/Home";
 import Header from "./Pages/Shared/Header/Header/Header";
 
 function App() {
     return (
-        <Router>
-            <Header></Header>
-            <Switch>
-                <Route exact path="/">
-                    <Home></Home>
-                </Route>
-                <Route path="/home">
-                    <Home></Home>
-                </Route>
-            </Switch>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Header></Header>
+                <Switch>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/home">
+                        <Home></Home>
+                    </Route>
+                </Switch>
+            </Router>
+        </AuthProvider>
     );
 }
 
