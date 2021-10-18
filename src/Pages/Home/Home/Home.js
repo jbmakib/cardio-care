3c573ab8805c1dbcba2341091a887d1ad4821d49
jbmakib <mdfoujolmobin@gmail.com>
2021-10-18 12:46:58 +0600

    home created

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container mx-auto">
+            <h2>This ish home</h2>
+        </div>
+    );
+};
+
+export default Home;
