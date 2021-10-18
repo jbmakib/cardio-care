0940ef00d3d8d6dea169c5723ae2bb717061e647
jbmakib <mdfoujolmobin@gmail.com>
2021-10-18 19:12:48 +0600

    login page updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,106 @@
 import React from "react";
+import { Link, useLocation, useHistory } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import Input from "../Input/Input";
+import LoginButton from "../LoginButton/LoginButton";
 
 const Login = () => {
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_URI = location.state?.from || "/";
+    const { signInWithGoogle } = useAuth();
+
+    const handleSignInWithProvider = (provider) => {
+        provider()
+            .then((result) => {
+                history.push(redirect_URI);
+                console.log(result.user);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
     return (
-        <div>
-            <h3>This ish login</h3>
+        <div className="container mx-auto px-4 h-full">
+            <div className="flex content-center items-center justify-center h-full">
+                <div className="w-full lg:w-4/12 px-4 py-8">
+                    <div className="relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg bg-red-200 border-0">
+                        <div className="rounded-t mb-0 px-6 pt-6 pb-4">
+                            <div className="text-center mb-3">
+                                <h6 className="text-red-600 text-sm font-bold">
+                                    Log in with
+                                </h6>
+                            </div>
+                            <div className="btn-wrapper text-center">
+                                <LoginButton
+                                    onClick={() =>
+                                        handleSignInWithProvider(
+                                            signInWithGoogle
+                                        )
+                                    }
+                                >
+                                    Google
+                                </LoginButton>
+                                <LoginButton>Github</LoginButton>
+                            </div>
+                            <hr className="mt-4 border-b-1 border-red-400" />
+                        </div>
+                        <div className="flex-auto px-4 lg:px-10 pt-0">
+                            <div className="text-red-500 text-center mb-3 font-bold">
+                                <small>Or Log in with credentials</small>
+                            </div>
+                            <form>
+                                <Input type="email" id="login-email-input">
+                                    Email
+                                </Input>
+                                <Input
+                                    type="password"
+                                    id="login-password-input"
+                                >
+                                    Password
+                                </Input>
+                                <div>
+                                    <label className="inline-flex items-center cursor-pointer">
+                                        <input
+                                            id="customCheckLogin"
+                                            type="checkbox"
+                                            className="form-checkbox text-red-800 ml-1 w-5 h-5"
+                                            style={{
+                                                transition: "all 0.15s ease 0s",
+                                            }}
+                                        />
+                                        <span className="ml-2 text-sm font-semibold text-red-700">
+                                            Remember me
+                                        </span>
+                                    </label>
+                                </div>
+                                <div className="text-center mt-6">
+                                    <button
+                                        className="bg-red-900 text-white active:bg-red-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                                        type="button"
+                                        style={{
+                                            transition: "all 0.15s ease 0s",
+                                        }}
+                                    >
+                                        Log In
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="px-4 lg:px-10 my-3">
+                            <small>
+                                Or{" "}
+                                <Link
+                                    to="/sign-up"
+                                    className="text-red-900 hover:underline"
+                                >
+                                    Create New Account
+                                </Link>
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
