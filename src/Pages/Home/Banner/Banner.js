e3a72a7e749f61fe492da694a414fe560750ea3d
jbmakib <mdfoujolmobin@gmail.com>
2021-10-19 09:06:43 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,17 +4,21 @@ import "./Banner.css";
 const Banner = () => {
     return (
         <div className="banner">
-            <div className="container mx-auto md:grid-cols-2 grid-cols-1 grid items-center">
+            <div className="container mx-auto lg:grid-cols-2 grid-cols-1 grid items-center">
                 <div>
-                    <div className="my-40">
+                    <div className="md:my-40 my-20">
                         <h1 className="text-2xl pl-1">
                             "Inspiring Better Health"
                         </h1>
-                        <h1 className="text-8xl font-bold text-red-600">
+                        <h1 className="text-6xl font-bold text-red-600 md:text-8xl">
                             Heart Healthy
                         </h1>
-                        <span className="text-3xl font-bold pl-1">Means</span>
-                        <h2 className="text-7xl font-bold">Body Healthy</h2>
+                        <span className="md:text-3xl font-bold pl-1 text-2xl">
+                            Means
+                        </span>
+                        <h2 className="text-5xl font-bold md:text-7xl">
+                            Body Healthy
+                        </h2>
                     </div>
                 </div>
             </div>
